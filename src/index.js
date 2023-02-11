be59fe3474894136a939a4057506e33255a591ce
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-02-11 18:10:25 +0200

    all update

--- src/index.js
+++ src/index.js
@@ -7,47 +7,49 @@ import {
   createMarkupCountry,
 } from './js/markup-country';
 
+const refs = {
+  inputEl: document.getElementById('search-box'),
+  listCountries: document.querySelector('.country-list'),
+  countyEl: document.querySelector('.country-info'),
+};
+
 let valuePromise = {};
 const DEBOUNCE_DELAY = 300;
 
-const inputEl = document.getElementById('search-box');
-const listCountries = document.querySelector('.country-list');
-const countyEl = document.querySelector('.country-info');
-
-inputEl.addEventListener('input', debounce(onCreate, DEBOUNCE_DELAY));
+refs.inputEl.addEventListener('input', debounce(onCreate, DEBOUNCE_DELAY));
 
 function onCreate() {
-  const value = inputEl.value.trim();
+  const value = refs.inputEl.value.trim();
   valuePromise = fetchCountries(value);
-  if (value.length <= 1) {
-    Notify.info('Too many matches found. Please enter a more specific name.');
-    return;
-  }
   valuePromise
     .then(countries => {
       if (countries.length === 1) {
         return countries.map(country => createMarkupCountry(country));
       }
-      return countries.reduce(
-        (markup, country) => createListMarkupCountries(country) + markup,
-        ''
+      if (countries.length <= 10) {
+        return countries.reduce(
+          (markup, country) => createListMarkupCountries(country) + markup,
+          ''
+        );
+      }
+      throw Notify.info(
+        'Too many matches found. Please enter a more specific name.'
       );
     })
-    .then(markupCountry)
+    .then(onMarkupCountry)
     .catch(onError);
-  inputEl.style.outlineColor = 'green';
+  refs.inputEl.style.outlineColor = 'green';
 }
 
-function markupCountry(markup) {
+function onMarkupCountry(markup) {
   valuePromise
     .then(countries => {
-      if (countries.length > 1 && countries.length <= 10) {
-        listCountries.innerHTML = markup;
-        countyEl.innerHTML = '';
-      } else {
-        countyEl.innerHTML = markup;
-        listCountries.innerHTML = '';
+      if (countries.length === 1) {
+        refs.countyEl.innerHTML = markup;
+        refs.listCountries.innerHTML = '';
       }
+      refs.listCountries.innerHTML = markup;
+      refs.countyEl.innerHTML = '';
     })
     .catch(onError);
 }
@@ -55,6 +57,6 @@ function markupCountry(markup) {
 function onError() {
   console.error('countries not found');
   Notify.failure('Oops, there is no country with that name');
-  markupCountry('<p class="country-error">countries not found</p>');
-  inputEl.style.outlineColor = 'red';
+  onMarkupCountry('<p class="country-error">countries not found</p>');
+  refs.inputEl.style.outlineColor = 'red';
 }
